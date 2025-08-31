ab72520dce0065573c03104fc84ff223754e5d68
Lucas Chicoski dos Santos <lucas.chicoski9@gmail.com>
2025-08-31 22:04:19 +0000

    alanisando dados netflix

--- src/core/read_file.py
+++ src/core/read_file.py
@@ -8,15 +8,21 @@ class ReadFile:
     
     
     def __init__(self):
+        self.filename = ''
         pass
     
     def readCSV(self) -> pd.DataFrame:
-        return pd.read_csv(f'{self.PATH_ARCHIVE}/Best_Games_of_All_Time.csv')
+        asd = os.listdir(path=self.PATH_ARCHIVE)
+        self.filename = asd[1]
+        return pd.read_csv(f'{self.PATH_ARCHIVE}/{self.filename}')
         
     
     def fetch_data(self, url:str):
-        urllib.request.urlretrieve(url, f'{self.PATH_ARCHIVE}/best-games-of-all-time.zip')
-        zipfile.ZipFile(f'{self.PATH_ARCHIVE}/best-games-of-all-time.zip').extractall(path=self.PATH_ARCHIVE)
+        urllib.request.urlretrieve(url, f'{self.PATH_ARCHIVE}/dataset.zip')
+        zipfile.ZipFile(f'{self.PATH_ARCHIVE}/dataset.zip').extractall(path=self.PATH_ARCHIVE)
+        asd = os.listdir(path=self.PATH_ARCHIVE)
+        self.filename = asd[1]
+        print(asd[1])
         # tarfile.open('files/dataset/best-games-of-all-time.zip').extractall(path=self.PATH_ARCHIVE)
         
     def verify_path(self) -> bool:
--- src/index.py
+++ src/index.py
@@ -6,23 +6,6 @@ if(__name__  == '__main__'):
 
     readFile: ReadFile = ReadFile()
     
-    if readFile.verify_path() == False:
-        readFile.fetch_data('https://www.kaggle.com/api/v1/datasets/download/arnavtomar18/best-games-of-all-time')
+    readFile.fetch_data('https://www.kaggle.com/api/v1/datasets/download/rohitgrewal/netflix-data')
+    print(readFile.readCSV())
     
-    
-    data_set:pd.DataFrame = readFile.readCSV()
-    
-    data_set['Launch_date'] = pd.to_datetime(data_set['Launch_date'])
-    data_set["Year"] = data_set['Launch_date'].dt.year
-    
-    data_set.plot(kind='line', x="Year", y="Metascore", figsize=(10,5) )
-    plt.show()
-    
-    # print(data_set['Launch_date'])
-    # print(data_set.head())
-    # print(data_set.columns)
-    
-    # print(readFile.verify_path())
-
-
-
